d62c4175bb699e94930d4432114ce97f445d7b07
saba <kananian.saba@gmail.com>
2025-05-03 17:14:44 +0000

    Setup base project

---
+++ src/config/index.ts
@@ -0,0 +1,15 @@
+import { readFileSync } from "node:fs";
+
+import { envs } from "./envs/index.js";
+import loggerConfig from "./logger/index.js";
+const pkg = JSON.parse(readFileSync("./package.json", { encoding: "utf8" }));
+
+export const config: Partial<TsED.Configuration> = {
+  version: pkg.version,
+  envs,
+  ajv: {
+    returnsCoercedValues: true
+  },
+  logger: loggerConfig
+  // additional shared configuration
+};
