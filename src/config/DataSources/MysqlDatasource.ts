1a1f3e492301b23df51f9ee2cbbc297385f19143
saba <kananian.saba@gmail.com>
2025-05-05 15:03:46 +0000

    debug

--- src/config/DataSources/MysqlDatasource.ts
+++ src/config/DataSources/MysqlDatasource.ts
@@ -10,19 +10,17 @@ import { fileURLToPath } from "url";
 dotenv.config();
 
 export const MYSQL_DATA_SOURCE = Symbol.for("MysqlDataSource");
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const entitiesPath = path.resolve(__dirname, "../../models/*.js");
-
-console.log("Entities path (resolved):", entitiesPath);
 
 registerProvider<DataSource>({
 	provide: MYSQL_DATA_SOURCE,
 	type: DataSource,
 	deps: [Logger],
 	async useAsyncFactory(logger: Logger) {
-		console.log("MysqlDatasourceProvider factory is running");
-		console.log("Entities paths =====>>>> ", [entitiesPath]);
+		const __filename = fileURLToPath(import.meta.url);
+		const __dirname = dirname(__filename);
+		const entitiesPath = path.resolve(__dirname, "../../models/*.js");
+		console.log("Entities path (resolved):", entitiesPath);
+
 		const MysqlDataSource = new DataSource({
 			type: "mysql",
 			entities: [`${__dirname}/../../models/*.js`],
