59bfa4c1d5bf269e379da4942d3ba3e596adca10
saba <kananian.saba@gmail.com>
2025-05-12 17:20:46 +0000

    tets error

--- src/controllers/rest/PairKeyController.ts
+++ src/controllers/rest/PairKeyController.ts
@@ -1,4 +1,5 @@
 import { Controller, Inject } from "@tsed/di";
+import { BadRequest } from "@tsed/exceptions";
 import { BodyParams, QueryParams } from "@tsed/platform-params";
 import { Get, Post } from "@tsed/schema";
 import { PairKey } from "src/models/PairKey.js";
@@ -10,6 +11,7 @@ export class PairKeyController {
 
 	@Post("/generate")
 	async generate(@BodyParams("posDeviceId") posDeviceId: string): Promise<PairKey> {
+		throw new BadRequest("Test error");
 		return await this.pairKeyService.generate(posDeviceId);
 	}
 
